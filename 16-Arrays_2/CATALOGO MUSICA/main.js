753a55b54bdc66746d6e82424a0f3e0d2333a60e
Rifi <121685704+pardalr@users.noreply.github.com>
2024-03-12 22:44:43 +0100

    Add files via upload

---
+++ 16-Arrays_2/CATALOGO MUSICA/main.js
@@ -0,0 +1,88 @@
+function borrarPrimero(){
+    bandas.shift();
+    document.getElementById("lista").innerHTML = "";
+    for (let i=0; i<=bandas.length-1; i++){
+        document.getElementById("lista").innerHTML += (i + " - " + bandas[i] + "<br>");
+    }
+}
+
+function borrarUltimo(){
+    bandas.pop();
+    document.getElementById("lista").innerHTML = "";
+    for (let i=0; i<=bandas.length-1; i++){
+        document.getElementById("lista").innerHTML += (i + " - " + bandas[i] + "<br>");
+    }
+}
+
+
+function borrarCambiar(){
+    
+    console.log("borrarCambiar");
+    let decision = prompt("¿Quieres borrar, cambiar o añadir banda?").toLocaleLowerCase();
+    if (decision == "borrar"){
+        console.log("borrar");
+        let index = parseInt(prompt("Indica la posición (del 0 al " + (indice - 1) +""));
+        bandas.splice(index, 1);
+    } else if (decision == "cambiar"){
+        let cambio = prompt("¿Qué banda quieres sustituir?");
+        let sustituir = prompt("¿Por cuál la quieres sustituir?");
+        let resultado = bandas.indexOf(cambio);
+        bandas[resultado] = sustituir;
+    } else if (decision == "añadir") {
+        let añadir = prompt("¿Qué banda quieres añadir?");
+        let posicion = parseInt(prompt("¿En qué posición del índice quieres añadirla?"));
+        let numero = parseInt(prompt("¿Cuántas bandas contíguas quieres eliminar?"));
+        bandas.splice(posicion, numero, añadir);
+    } else {
+        alert("Elige una de las tres opciones, carajo!");
+    }
+
+    document.getElementById("lista").innerHTML = "";
+    for (let i=0; i<=bandas.length-1; i++){
+        document.getElementById("lista").innerHTML += (i + " - " + bandas[i] + "<br>");
+    }
+
+}
+
+
+
+function consultarIndice(){
+    let cambio = prompt("¿De qué banda quieres conocer el índice?");
+    let resultado = bandas.indexOf(cambio);    
+    document.getElementById("lista").innerHTML = ("El índice de " + cambio + " es " + resultado);
+}
+
+
+
+
+function consultarBanda(){
+    
+    let cambio = prompt("A qué banda corresponde el índice: ");
+    let resultado = bandas[cambio];    
+    document.getElementById("lista").innerHTML = ("El índice " + cambio + " corresponde a " + resultado);
+
+}
+
+function mostrarTodos(){
+
+
+    let text = "";
+    bandas.forEach(myFunction);
+
+    document.getElementById("lista").innerHTML = text;
+    
+    function myFunction(item, index) {
+    text += index + ": " + item + "<br>"; 
+    }
+
+
+
+
+}
+
+
+const bandas = ["Nirvana", "Beatles", "AC/DC", "Rolling Stones", "U2", "Beach Boys"];
+let indice = bandas.length;
+for (let i=0; i<=bandas.length-1; i++){
+    document.getElementById("lista").innerHTML += (i + " - " + bandas[i] + "<br>");
+}
